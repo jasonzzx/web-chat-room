90fdffea0e1435d33cb75c3a831507f331333c30
Jason Zhong <jasonzzx@gmail.com>
2017-06-19 00:37:11 -0400

    setup vuex store objects

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,12 +3,21 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import createLogger from 'vuex/dist/logger';
+import state from './state';
+import * from actions from './actions';
+import mutations from './mutations';
 
 Vue.use(Vuex);
 
 const debug = process.env.NODE_ENV !== 'production';
 
 export default new Vuex.Store({
+  actions,
+
+  mutations,
+
+  state,
+
   strict: debug,
 
   plugins: debug ? [createLogger()] : [],
